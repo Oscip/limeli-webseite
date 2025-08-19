c4f219f077de3bd3b2879dba72099266526b3691
Oscip <oscar@peachy.ch>
2025-08-19 21:33:23 +0200

    Fix: gh-pages issue
    - Router problem solved
    - changed from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home.jsx";
 import Drinks from "./pages/Drinks.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import Contact from "./pages/Contact.jsx";
 import NavbarComponent from "./Components/NavbarComponent.jsx";
-import NotFound from "./pages/NotFound.jsx";
 import FooterComponent from "./Components/FooterComponent.jsx";
 import Legal_Notice from "./pages/Legal_Notice.jsx";
 import Terms_and_Condition from "./pages/Terms_and_Conditions.jsx";
@@ -19,6 +18,7 @@ import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
 function App() {
     return (
         <CartProvider>
+            <Router>
                 <NavbarComponent />
                 <div>
                     <Routes>
@@ -32,10 +32,11 @@ function App() {
                         <Route path="/terms-and-conditions" element={<Terms_and_Condition />} />
                         <Route path="/terms-of-use" element={<TermsOfUse />} />
                         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                        <Route path="/*" element={<NotFound />} />
+                        <Route path="*" element={<Navigate to="/" />} />
                     </Routes>
                 </div>
                 <FooterComponent />
+            </Router>
         </CartProvider>
     );
 }
